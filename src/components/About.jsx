cea87718b9d9c79d7b1e1307eae24cb5f668d2b7
Vikas <vikaskumarr365@gmail.com>
2025-06-20 16:22:42 +0100

    small fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
-
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
-            </div>
-            <p className='text-ml mt-20'>I am a Frontend developer with expertise in HTML5, CSS3, Tailwind, Bootstrap, JavaScript, and ReactJs. Additionally, I have a basic understanding of Node.js and I'm currently learning Next.js and Redux. I'm always eager to explore new technologies and I enjoy keeping up-to-date with the latest trends and best practices in web development. I love reading books and making responsive websites. I believe that creating visually appealing and functional websites is both an art and a science, and I am constantly striving to improve my skills in both areas.</p>
-
-            <br />
-
-            <p className='text-ml'>I'm always looking for new ways to improve my skills as a web developer, and one of my favorite ways to do so is by watching documentaries and reading books and podcasts about the latest trends in the industry.These resources not only help me to stay up-to-date with the latest news and advancements in the field, but they also provide valuable insights on time management, working hard, and other important topics that help me to improve as a professional.</p>
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-0">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
+        <p className="text-ml mt-20">
+          I am a Frontend developer with expertise in HTML5, CSS3, Tailwind,
+          Bootstrap, JavaScript, and ReactJs. Additionally, I have a basic
+          understanding of Node.js and I'm currently learning Next.js and Redux.
+          I'm always eager to explore new technologies and I enjoy keeping
+          up-to-date with the latest trends and best practices in web
+          development. I love reading books and making responsive websites. I
+          believe that creating visually appealing and functional websites is
+          both an art and a science, and I am constantly striving to improve my
+          skills in both areas.
+        </p>
+
+        <br />
+
+        <p className="text-ml">
+          I'm always looking for new ways to improve my skills as a web
+          developer, and one of my favorite ways to do so is by watching
+          documentaries and reading books and podcasts about the latest trends
+          in the industry.These resources not only help me to stay up-to-date
+          with the latest news and advancements in the field, but they also
+          provide valuable insights on time management, working hard, and other
+          important topics that help me to improve as a professional.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,40 +1,54 @@
-import React from 'react';
-import heroimage from '../assets/heroimage.png'
+import React from "react";
+import heroimage from "../assets/heroimage.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div id="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800">
+    <div
+      id="home"
+      className="h-screen w-full bg-gradient-to-b from-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-3xl sm:text-4xl font-bold text-white">
+            Frontend Developer
+          </h2>
+          <p className="py-4 max-w-xl text-white">
+            Hello visitors, I'm vikas Looking an opportunity as a frontend
+            developer. I am dedicated to creating aesthetically pleasing and
+            highly functional web applications. I am also enthusiastic about
+            exploring new technologies and staying up-to-date with industry best
+            practices. If you're interested in seeing some of my work, please
+            visit my GitHub.Feel free to connect with me on LinkedIn and my
+            blog, which is available on Hashnode. Thank you.
+          </p>
 
-    <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-     <div className="flex flex-col justify-center h-full">
-      <h2 className="text-3xl sm:text-4xl font-bold text-white">
-        Frontend Developer
-      </h2>
-      <p className="py-4 max-w-xl text-white">
-      Hello visitors, I'm vikas Looking an opportunity as a frontend developer. I am dedicated to creating aesthetically pleasing and highly functional web applications. I am also enthusiastic about exploring new technologies and staying up-to-date with industry best practices.
-      If you're interested in seeing some of my work, please visit my GitHub.Feel free to connect with me on LinkedIn and my blog, which is available on Hashnode. Thank you.
-      </p>
+          <div>
+            {/* <Link
+              to="Projects"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-2xl md:rounded-2xl rounded-mg bg-sky-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link> */}
+          </div>
+        </div>
 
-      <div>
-        <Link to="Projects" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-2xl md:rounded-2xl rounded-mg bg-sky-500 cursor-pointer">
-          Portfolio
-          <span className="group-hover:rotate-90 duration-300">
-          <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
-          </span>
-        </Link>
+        <div>
+          <img
+            src={heroimage}
+            alt="profile"
+            className="rounded mx-auto w-full md:w-1/2"
+          />
+        </div>
       </div>
-     </div>
-
-     <div>
-      <img src={heroimage} alt="profile" className="rounded mx-auto w-1/2 md:w-1/2 " />
-     </div>
-
-    </div>
-
     </div>
   );
-}
+};
 
 export default Home;
